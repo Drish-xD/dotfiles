e664f774be5a988208efb31796970c472487017b
Drish-xD <drish.xd@gmail.com>
2022-03-02 17:55:27 +0530

    init dotfile

---
+++ .scripts/mpd/mpd-next.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+mpc status &> /dev/null
+if test $? -eq 1
+then
+	exit
+fi
+
+current=$(mpc status | head -1)
+next=$(mpc playlist | grep -A 1 "$current" | sed -e "s/\<$current\>//g")
+
+if [ "$next" = "" ]; then
+	next=$(mpc playlist | head -1)
+	echo -e " \033[1;32m[]$next\n"
+else
+	echo -e " \033[1;32m[]$next\n"
+fi
