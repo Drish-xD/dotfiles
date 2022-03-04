318413092086849cacef8a01e179cb051e7c9b77
Drish-xD <drish.xd@gmail.com>
2022-03-04 06:21:59 +0530

    refactor

---
+++ .scripts/Polybar/zsrcoll-text.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# see man zscroll for documentation of the following parameters
+zscroll -l 25 \
+        --delay 0.1 \
+        --scroll-padding "  " \
+        --match-command "`dirname $0`/player_status.sh --status" \
+        --match-text "Playing" "--scroll 1" \
+        --match-text "Paused" "--scroll 0" \
+        --update-check true "`dirname $0`/player_status.sh" &
+
+wait
+
